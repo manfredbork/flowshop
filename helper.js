f624e4e43c98f437b1551ec7da162f5812401588
manfredbork <bork.ma@t-online.de>
2013-06-23 16:56:27 +0200

    Added helper functions

--- helper.js
+++ helper.js
@@ -6,9 +6,20 @@ function Helper() {
 Helper.exports = Helper;
 
 /*--------------------------------------------------------*/
+/* Check if data is two dimensional array
+/*--------------------------------------------------------*/
+Helper.prototype.isValidData = function(data) {
+    return data && data.length > 0 && data[data.length - 1].length > 0;
+};
+exports.isValidData = Helper.prototype.isValidData;
+
+/*--------------------------------------------------------*/
 /* Basic makespan implementation
 /*--------------------------------------------------------*/
 Helper.prototype.makespan = function(data) {
+    if(!Helper.prototype.isValidData(data)) {
+        return 0;
+    }
     var grid = [];
     for(var i = 0; i < data.length; i++) {
         grid[i] = [];
@@ -19,3 +30,47 @@ Helper.prototype.makespan = function(data) {
     return grid[data.length - 1][data[data.length - 1].length - 1];
 };
 exports.makespan = Helper.prototype.makespan;
+
+/*--------------------------------------------------------*/
+/* Toggle specified values in array
+/*--------------------------------------------------------*/
+Helper.prototype.toggle = function(data, firstPosition, secondPosition) {
+    if(!Helper.prototype.isValidData(data)) {
+        return data;
+    }
+    var clone = data.slice(), value = clone[firstPosition - 1];
+    clone[firstPosition - 1] = clone[secondPosition - 1];
+    clone[secondPosition - 1] = value;
+    return clone;
+};
+exports.toggle = Helper.prototype.toggle;
+
+/*--------------------------------------------------------*/
+/* Remove field in array
+/*--------------------------------------------------------*/
+Helper.prototype.remove = function(data, position) {
+    if(!Helper.prototype.isValidData(data)) {
+        return data;
+    }
+    return data.slice(0, position - 1).concat(data.slice(position));
+};
+exports.remove = Helper.prototype.remove;
+
+/*--------------------------------------------------------*/
+/* Insert value after specified field in array
+/*--------------------------------------------------------*/
+Helper.prototype.insertAfter = function(data, position, value) {
+    if(!Helper.prototype.isValidData(data)) {
+        return data;
+    }
+    return data.slice(0, position).concat([value]).concat(data.slice(position));
+};
+exports.insertAfter = Helper.prototype.insertAfter;
+
+/*--------------------------------------------------------*/
+/* Insert value before specified field in array
+/*--------------------------------------------------------*/
+Helper.prototype.insertBefore = function(data, position, value) {
+    return Helper.prototype.insertAfter(data, position - 1, value);
+};
+exports.insertBefore = Helper.prototype.insertBefore;
