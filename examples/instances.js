6eecd63cbe03ce0b0373b5a616621a1305071d25
manfredbork <bork.ma@t-online.de>
2013-08-09 21:13:31 +0200

    Use number generator with seed

--- examples/instances.js
+++ examples/instances.js
@@ -1,9 +1,13 @@
 var instances = require('./../taillard');
 var NEH = require('./../neh');
+var seed = require('./../seed-random');
 
 // Iterate over filtered Taillard instances
 var filteredInstances = instances.filter(50, 20);
 for(var i = 0; i < filteredInstances.length; i++) {
+
+    // Overwrite Math.random by number generator with seed
+    seed(filteredInstances[i].initialSeed, true);
 
     console.log('               name:', filteredInstances[i].name);
     console.log('     number of jobs:', filteredInstances[i].numberOfJobs);
