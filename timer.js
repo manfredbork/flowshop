084a2f0e06fca3a39b6aac5b6966b696a9162326
manfredbork <bork.ma@t-online.de>
2013-08-10 23:03:18 +0200

    Added reset parameter for timer

--- timer.js
+++ timer.js
@@ -8,12 +8,14 @@ Timer.prototype.reset = function() {
 };
 exports.reset = Timer.prototype.reset;
 
-// Measure time between last call and now
-Timer.prototype.diff = function(digits) {
+// Measure time since last reset
+Timer.prototype.diff = function(reset, digits) {
     if(Timer.prototype.time && Timer.prototype.time.length == 2) {
         var start = +(Timer.prototype.time.join('.'));
         var now = +(process.hrtime().join('.'));
-        Timer.prototype.time = process.hrtime();
+        if(reset) {
+            Timer.prototype.time = process.hrtime();
+        }
         if(digits) {
             return Math.floor((now - start) * Math.pow(10, digits)) / Math.pow(10, digits);
         } else {
