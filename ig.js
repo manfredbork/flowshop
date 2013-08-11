fa8222dac85f84bd19819b5130a8833b237f2245
manfredbork <bork.ma@t-online.de>
2013-08-11 11:52:57 +0200

    Added temperature and criterion as variables

--- ig.js
+++ ig.js
@@ -9,16 +9,25 @@ function IG() {
 util.inherits(IG, Flowshop);
 exports.makespan = IG.prototype.makespan;
 
+// Set criterion milliseconds
+IG.prototype.setCriterionMilliseconds = function(ms) {
+    IG.prototype.criterionMilliseconds = ms;
+};
+IG.prototype.criterionMilliseconds = 60;
+exports.setCriterionMilliseconds = IG.prototype.setCriterionMilliseconds;
+
 // Set T parameter by level
 IG.prototype.setT = function(level) {
     IG.prototype.T = Helper.T[level - 1];
 };
+IG.prototype.T = IG.prototype.setT(1);
 exports.setT = IG.prototype.setT;
 
 // Set d parameter by level
 IG.prototype.setd = function(level) {
     IG.prototype.d = Helper.d[level - 1];
 };
+IG.prototype.d = IG.prototype.setd(1);
 exports.setd = IG.prototype.setd;
 
 // NEH initialization
@@ -74,6 +83,8 @@ IG.prototype.iterativeImprovementInsertion = function(pi) {
 
 // Iterated Greedy heuristic
 IG.prototype.order = function(data) {
+    var temperature = Helper.temperature(data, IG.prototype.T);
+    var criterion = Helper.criterion(data, IG.prototype.criterionMilliseconds);
     var pi = IG.prototype.initializationNEH(data);
     pi = IG.prototype.iterativeImprovementInsertion(pi);
     return pi;
