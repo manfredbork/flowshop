2bbc7b7181b72256f07774591960d593e337ca9c
manfredbork <bork.ma@t-online.de>
2013-08-21 18:10:29 +0200

    Refactored version

--- index.js
+++ index.js
@@ -11,17 +11,27 @@ var IG = require('./src/ig');
 util._extend(Math, Random.prototype);
 
 // Initialization
-var name = 'ta005';
 var importer = new Importer();
-var metaData = importer.loadMetaData(name);
-var matrixData = importer.loadMatrixData(name);
+var names = ['ta001', 'ta002', 'ta003'];
 var neh = new NEH();
 var ig = new IG();
 
-// Set initial seed
-Math.initialSeed(metaData.initialSeed);
+for(var i = 0; i < names.length; i++) {
 
-// Run algorithms
-console.log('INSTANCE NAME', name);
-console.log(' NEH MAKESPAN', neh.run(matrixData).makespan());
-console.log('  IG MAKESPAN', ig.run(matrixData).makespan());
+    // Instance data
+    var name = names[i];
+    var metaData = importer.loadMetaData(name);
+    var matrixData = importer.loadMatrixData(name);
+
+    // Set initial seed
+    Math.initialSeed(metaData.initialSeed);
+
+    // Run algorithms
+    console.log('INSTANCE NAME', metaData.name);
+    console.log('         JOBS', metaData.jobs);
+    console.log('     MACHINES', metaData.machines);
+    console.log(' NEH MAKESPAN', neh.run(matrixData).makespan());
+    console.log('  IG MAKESPAN', ig.run(matrixData).makespan());
+    console.log('-----------------------------');
+
+}
